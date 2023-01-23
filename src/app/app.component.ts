77d13893beae135a1c91da8861cfb52f8642c14c
Jose David Rivera Sanchez <davidrivera565@gmail.com>
2023-01-23 14:18:31 -0600

    Laboratorio 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Adopta una mascota';
+  descripcion: string='A través de la historia, las mascotas han sido grandes compañeras de la vida de las personas, compartiendo alegrías, pero también ayudando a superar momentos difíciles'
+  img='https://thecatapi.com/api/images/get?format=src&type=gif'
+}
